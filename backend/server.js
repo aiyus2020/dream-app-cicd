aada471414d57e2b2fc6d663af12708e3e04efe8
aiyus <aiyusboss@gmail.com>
2025-08-02 17:56:34 +0100

    dwdf

--- backend/server.js
+++ backend/server.js
@@ -56,5 +56,5 @@ app.delete('/api/destinations/:id', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servesr running on port ${port}`);
+  console.log(`Servsr running on port ${port}`);
 });
