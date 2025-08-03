f420fa012e55260b6e6958a4a290c2c157f6f045
aiyus <aiyusboss@gmail.com>
2025-08-03 13:06:23 +0100

    dwf

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,38 @@
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
+import parserBabel from "@babel/eslint-parser"; // JSX-aware parser
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,jsx,mjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: parserBabel,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      "react/jsx-uses-react": "warn",
+      "react/jsx-uses-vars": "warn",
+    },
+  },
 ]);
+
