1604d179d8974681534cef422576759354804871
aiyus <aiyusboss@gmail.com>
2025-08-03 08:18:18 +0100

    efef

--- backend/server.js
+++ backend/server.js
@@ -56,5 +56,5 @@ app.delete('/api/destinations/:id', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servsr running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
