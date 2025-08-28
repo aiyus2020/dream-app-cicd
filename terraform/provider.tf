b744b7c301a959e2526d43a0c2bf1a047ac57d80
aiyus <aiyusboss@gmail.com>
2025-08-28 12:06:35 +0100

    ty

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,11 @@
 terraform {
-#   backend "s3" {
-#     bucket       = "aiyus-dream-app-setup"
-#     key          = "backend/terraform.tfstate"
-#     region       = var.aws_region
-#     profile      = "default"
-#     use_lockfile = true
-#   }
+  backend "s3" {
+    bucket       = "aiyus-dream-app-setup"
+    key          = "backend/terraform.tfstate"
+    region       = var.aws_region
+    profile      = "default"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
