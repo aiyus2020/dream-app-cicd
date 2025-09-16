421156dcd00a302a3a316997f4d843f9650c6b3f
aiyus <aiyusboss@gmail.com>
2025-09-16 14:15:03 +0100

    wfeff

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -19,7 +19,7 @@ variable "subnet_cidr" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-    default     = "t3.micro"
+    default     = "t2.micro"
 }
 
 variable "project_name" {
