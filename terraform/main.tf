35c933698f730b9e3466d6828a2fa62399660819
aiyus <aiyusboss@gmail.com>
2025-09-16 11:47:15 +0100

    addd

--- terraform/main.tf
+++ terraform/main.tf
@@ -179,18 +179,3 @@ resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
     InstanceId = aws_instance.ec2.id
   }
 }
-
-# --------------------------
-# Outputs
-# --------------------------
-output "ec2_public_ip" {
-  value = aws_instance.ec2.public_ip
-}
-
-output "ec2_public_dns" {
-  value = aws_instance.ec2.public_dns
-}
-
-output "ssh_command" {
-  value = "ssh -i terraform-deploy.pem ubuntu@${aws_instance.ec2.public_ip}"
-}
