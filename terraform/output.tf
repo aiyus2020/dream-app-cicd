63f0868b167c48557cc21157376a217e3a0d4fcd
aiyus <aiyusboss@gmail.com>
2025-09-21 14:37:46 +0100

    dwd

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,17 +8,20 @@ output "subnet_id" {
   value = aws_subnet.main.id
 }
 
-output "ec2_public_ip" {
-  value = data.aws_eip.existing_eip.public_ip
-}
+
 
 output "ec2_public_dns" {
   description = "value for ec2_public_dns"
   value = aws_instance.ec2.public_dns
 }
+output "ec2_public_ip" {
+  value = data.aws_eip.existing_eip.public_ip
+}
+
 output "ssh_output" {
   value = "ssh -i ${var.key_name}.pem ubuntu@${data.aws_eip.existing_eip.public_ip}"
 }
+
 
 
 
